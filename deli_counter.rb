2359acab1997439bec2271ffa37a9ae11d07ce72
Vanessa Toro <vanetoro84@gmail.com>
2018-03-23 00:19:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,5 +5,8 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.empty?
     puts  "The line is currently empty."
+  else
+    katz_deli.each do |customer, index|
+      puts "The line is currently#{"
   end
 end
