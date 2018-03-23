36ca9f68c3d7ce483b53f429c70d41a6bedfbca3
Vanessa Toro <vanetoro84@gmail.com>
2018-03-23 01:02:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,6 +7,8 @@ def line(deli)
   if deli.size > 0
     deli.each_with_index do |customer, place|
     deli_line << "#{place}. #{customer}"
+  end
+    puts "The line is currently: #{deli_line.join}"
   else deli.empty?
     puts  "The line is currently empty."
   end
