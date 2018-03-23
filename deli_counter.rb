b2d5b81743222042da42a6c72f8454418d2cba5f
Vanessa Toro <vanetoro84@gmail.com>
2018-03-23 01:18:59 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+katz_deli = []
+
+
+def line(deli)
+  deli_line = []
+  if deli.size > 0
+    deli.each_with_index do |customer, place|
+    deli_line << " #{place + 1}. #{customer}"
+  end
+    puts "The line is currently:#{deli_line.join}"
+  else deli.empty?
+    puts  "The line is currently empty."
+  end
+end
+
+def take_a_number(line, customer)
+  line.push(customer)
+  puts "Welcome, #{customer}. You are number #{line.size} in line."
+end 
+
+def now_serving(line)
+  if line.empty?
+    puts "There is nobody waiting to be served!"
+  else
+  serving = line.shift
+  puts "Currently serving #{serving}."
+end
+end
